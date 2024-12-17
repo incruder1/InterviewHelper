2bb63bd9d8673a89ffea448b3200df5f79aedd9c
Dev Johri <56020041+incruder1@users.noreply.github.com>
2024-12-17 21:24:04 +0530

    solved dev bugs

--- components/ui/Spinner.jsx
+++ components/ui/Spinner.jsx
@@ -1,10 +1,9 @@
 // Spinner.js
 import React from "react";
 
-const Spinner = ({ size = "w-12 h-12", color = "border-t-4 border-t-blue-500", speed = "animate-spin" }) => {
+export const Spinner = ({ size = "w-12 h-12", color = "border-t-4 border-t-blue-500", speed = "animate-spin" }) => {
   return (
     <div className={`border-4 border-gray-200 rounded-full ${color} ${speed} ${size}`} />
   );
 };
 
-export { Spinner };
